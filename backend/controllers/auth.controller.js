3f51a87aca346e0a611cc2a25ef3a42af94cc24c
Mohammed Jawwad <jawwadsyed5786@gmail.com>
2024-12-06 21:18:53 +0530

    Initial commit

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = async (res, res) => {
+    
+}
+
+export const login = async (res, res) => {
+    res.send("login route");
+}
+
+export const logout = async (res, res) => {
+    res.send("logout route");
+}
